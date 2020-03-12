24d449342cb008bfffc73ca749762388f2f63ab9
Sang-Yeoop <tkdduq8260@gmail.com>
2020-03-12 16:06:00 +0900

    Feat : 10871

--- src/Implemetation/Application.java
+++
@@ -1,9 +0,0 @@
-package Implemetation;
-
-public class Application {
-
-    public static void main(String[] args)
-    {
-        Problem9498.run();
-    }
-}
---
+++ src/Implemetation/Main.java
@@ -0,0 +1,12 @@
+package Implemetation;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        Problem10871.run();
+    }
+}
