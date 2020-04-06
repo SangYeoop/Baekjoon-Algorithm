d22061024cbb84d53457a30ad7f59938ac5a3e05
SangYeoop <tkdduq8260@gmail.com>
2020-04-06 21:05:16 +0900

    1939번 틀렸습니다

--- src/Implemetation/Problem1939.java
+++ src/Implemetation/Problem1939.java
@@ -4,63 +4,84 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Scanner;
 
-public class Problem1939 {
-    private LinkedList<Node> queue;
-    private int maxWeight;
-    private int factory1;
-    private int factory2;
-
-    public static void run() {
-        Problem1939 problem = new Problem1939();
-        Scanner sc = new Scanner(System.in);
-        int nodeNum = sc.nextInt();
-        int pathNum = sc.nextInt();
-        int[][] paths = new int[pathNum][3];
-
-        for (int i = 0; i < pathNum; i++) {
-            for (int j = 0; j < 3; j++) {
-                paths[i][j] = sc.nextInt();
-            }
-        }
-        problem.factory1 = sc.nextInt();
-        problem.factory2 = sc.nextInt();
-    }
-
-    class Node {
-        ArrayList<Node> nextNodes;
-        boolean isSearched;
-        boolean isFactory;
-        int pathValue;
-        int weight;
-
-        Node (boolean isFactory) {
-            this.isFactory = isFactory;
-        }
-
-        Node(ArrayList<Node> nextNodes, boolean isFactory) {
-            this.nextNodes = nextNodes;
-            this.isFactory = isFactory;
-            isSearched = false;
-            pathValue = 0;
-            weight = 0;
-        }
-
-        void setMaxWeight() {
-            if (isFactory == true)
-                isSearched = true;
-            if (isSearched == true)
-                return;
-            for (int i = 0; i < nextNodes.size(); i++) {
-                Node node = nextNodes.get(i);
-                int nextNodeWeight = weight;
-                if(node.pathValue < weight) {
-                    nextNodeWeight = node.pathValue;
-                }
-                maxWeight = nextNodeWeight;
-                queue.add(node);
-            }
-            isSearched = true;
-        }
-    }
-}
-
+//public class Main {
+//    LinkedList<Node> queue;
+//    int maxWeight;
+//
+//    Main() {
+//        queue = new LinkedList<Node>();
+//        maxWeight = 0;
+//    }
+//    void queueSearch() {
+//        while(true) {
+//            if (!queue.isEmpty())
+//                queue.poll().setMaxWeight(this);
+//            else
+//                break;
+//        }
+//        System.out.println(maxWeight);
+//    }
+//
+//    public static void main() {
+//        Main problem = new Main();
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        Node[] node = new Node[n + 1];
+//        for (int i = 1; i <= n; i++)
+//            node[i] = new Node();
+//        int m = sc.nextInt();
+//        for (int i = 0; i < m; i++) {
+//            int island1 = sc.nextInt();
+//            int island2 = sc.nextInt();
+//            int pathValue = sc.nextInt();
+//            node[island1].nodes.add(node[island2]);
+//            node[island2].nodes.add(node[island1]);
+//            node[island1].pathValue.add(pathValue);
+//            node[island2].pathValue.add(pathValue);
+//        }
+//        int firstIsland = sc.nextInt();
+//        int lastIsland = sc.nextInt();
+//        node[firstIsland].weight = Integer.MAX_VALUE;
+//        node[lastIsland].isLast = true;
+//        problem.queue.add(node[firstIsland]);
+//        problem.queueSearch();
+//    }
+//}
+//
+//class Node{
+//    ArrayList<Node> nodes;
+//    ArrayList<Integer> pathValue;
+//    boolean isSearched;
+//    boolean isLast;
+//    int weight;
+//
+//    Node() {
+//        weight = 0;
+//        isSearched = false;
+//        isLast = false;
+//        nodes = new ArrayList<Node>();
+//        pathValue = new ArrayList<Integer>();
+//    }
+//
+//    void setMaxWeight(Main problem) {
+//        if (isLast) {
+//            if (problem.maxWeight < weight)
+//                problem.maxWeight = weight;
+//            return ;
+//        }
+//        if (isSearched == false) {
+//            for (int i = 0; i < nodes.size(); i++) {
+//                if (weight > pathValue.get(i)) {
+//                    if (nodes.get(i).weight < pathValue.get(i))
+//                        nodes.get(i).weight = pathValue.get(i);
+//                } else {
+//                    if (nodes.get(i).weight < pathValue.get(i))
+//                        nodes.get(i).weight = pathValue.get(i);
+//                }
+//                problem.queue.add(nodes.get(i));
+//            }
+//            isSearched = true;
+//        }
+//    }
+//}
+//
