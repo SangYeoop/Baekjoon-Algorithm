990b5be954f79f552c1f0ee435689794779ddfb0
SangYeop <tkdduq8260@gmail.com>
2020-04-20 20:00:36 +0900

    Feat: 10989 & 1236

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -1,19 +1,8 @@
 package Implemetation;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Problem2750.run();
+    public static void main(String[] args){
     }
 }
-
-
-
