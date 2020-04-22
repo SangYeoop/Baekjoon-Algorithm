ed3eb3d6734355151bbe53801122cbffdbef8dba
SangYeop <tkdduq8260@gmail.com>
2020-04-22 20:33:43 +0900

    Feat : 2606

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -1,11 +1,9 @@
 package Implemetation;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Problem2606.run();
     }
 }
-
-
---
+++ src/Implemetation/Problem2606.java
@@ -0,0 +1,40 @@
+package Implemetation;
+
+import java.util.Scanner;
+
+public class Problem2606 {
+    public static void run() {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        int edge = sc.nextInt();
+
+        int[][] adj = new int[num + 1][num + 1];
+        boolean[] searched = new boolean[num + 1];
+
+        for (int i = 0; i < edge; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            adj[from][to] = 1;
+            adj[to][from] = 1;
+        }
+
+        int start = 1;
+        int result = search(adj, start, num, searched);
+        System.out.println(result - 1);
+    }
+
+    public static int search(int[][] adj, int start, int num, boolean[] searched) {
+        int result = 0;
+        if (searched[start])
+            return 0;
+        result++;
+        searched[start] = true;
+        for(int i = 1; i <= num; i++) {
+            if (adj[start][i] == 1 && i != start) {
+                result += search(adj, i, num, searched);
+            }
+        }
+        return result;
+    }
+}
