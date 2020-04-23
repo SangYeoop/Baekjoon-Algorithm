bca8e63541bd19f8395d2fb80b436fac30aa3093
SangYeop <tkdduq8260@gmail.com>
2020-04-23 15:11:37 +0900

    Feat : 2178

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -1,10 +1,12 @@
 package Implemetation;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Problem1012.run();
+        Problem2178.run();
     }
 }
+
 
