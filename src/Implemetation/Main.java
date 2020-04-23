a00f6b353ff8324e7b3e93eb750495b4b1bffc32
SangYeop <tkdduq8260@gmail.com>
2020-04-23 12:57:13 +0900

    Feat : 1012

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -1,12 +1,10 @@
 package Implemetation;
 
-import java.util.Comparator;
 import java.util.Scanner;
-import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Problem2667.run();
+        Problem1012.run();
     }
 }
 
