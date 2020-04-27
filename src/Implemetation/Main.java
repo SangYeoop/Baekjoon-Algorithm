e806251687ea7693283fe834f154705791be0287
Sang-Yeoop <tkdduq8260@gmail.com>
2020-04-27 22:50:31 +0900

    Feat : 18258

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -2,12 +2,10 @@ package Implemetation;
 
 import java.io.*;
 import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Problem1874.run();
+        Problem18258.run();
     }
 }
 
