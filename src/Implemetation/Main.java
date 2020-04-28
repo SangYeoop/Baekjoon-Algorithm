752e6d8a27bc205766ac1e2c95a5fb04c1915a3a
SangYeop <tkdduq8260@gmail.com>
2020-04-28 16:54:12 +0900

    Feat : 1065

--- src/Implemetation/Main.java
+++ src/Implemetation/Main.java
@@ -1,13 +1,9 @@
 package Implemetation;
 
-import java.io.*;
-import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Problem1874.run();
+    public static void main(String[] args) {
     }
 }
 
