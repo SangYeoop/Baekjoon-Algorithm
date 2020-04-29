169adcbe145a41c00ac131e0364adb383c25bc2d
Sang-Yeoop <tkdduq8260@gmail.com>
2020-04-29 18:54:08 +0900

    Feat : 15651

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,39 @@
 import BOJ.Problem1915;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Problem1915.run();
+    public static void main(String[] args) throws Exception {
+        Problem15651.run();
     }
 }
 
+class Problem15651 {
 
+    public static void run() throws Exception {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] selected = new int[m];
+        dfs(selected, 0, n, m, bufferedWriter);
+        bufferedWriter.flush();
+    }
+
+    public static void dfs(int[] selected, int depth, int n, int m, BufferedWriter bw) throws Exception {
+        if (depth == m) {
+            for (int i = 0; i < selected.length; i++) {
+                bw.write(selected[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            selected[depth] = i;
+            dfs(selected, depth + 1, n, m, bw);
+        }
+    }
+}
