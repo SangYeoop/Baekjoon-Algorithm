0c44d1ba0b3b350f26c0a7b4ae60ee0b7a849282
SangYeoop <tkdduq8260@gmail.com>
2020-05-06 09:41:14 +0900

    Feat : 파일명정렬

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import Porgrammers.파일명정렬;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        파일명정렬 a = new 파일명정렬();
+        String[] s = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
+        s = a.solution(s);
 
+        for (int i = 0; i  < s.length; i++){
+            System.out.println(s[i]);
+        }
     }
 }
 
---
+++ src/Porgrammers/파일명정렬.java
@@ -0,0 +1,76 @@
+package Porgrammers;
+
+import java.util.Comparator;
+import java.util.Vector;
+
+class File {
+    public String head;
+    public String number;
+    public String name;
+
+    File(String head, String number, String name) {
+        this.head = head;
+        this.number = number;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Head : " + head + " Number :" + number + " File Name : " + name + "\n";
+    }
+}
+class FileComparator implements Comparator<File>{
+    @Override
+    public int compare(File o1, File o2) {
+        if (o1.head.toLowerCase().compareTo(o2.head.toLowerCase()) > 0) {
+            return 1;
+        }
+        else if (o1.head.toLowerCase().compareTo(o2.head.toLowerCase()) == 0){
+            if(Integer.parseInt(o1.number) > Integer.parseInt(o2.number))
+                return 1;
+            else if(Integer.parseInt(o1.number) == Integer.parseInt(o2.number))
+                return 0;
+            else
+            return -1;
+        }
+        return -1;
+    }
+}
+
+public class 파일명정렬 {
+    public String[] solution(String[] files) {
+        String[] answers;
+        Vector<File> vec = new Vector<>();
+        for(int i = 0; i < files.length; i++) {
+            StringBuilder head = new StringBuilder();
+            StringBuilder num = new StringBuilder();
+            for (int j = 0; j < files[i].length(); j++) {
+                head.append(files[i].charAt(j));
+                if(Character.isDigit(files[i].charAt(j))) {
+                    num.append(files[i].charAt(j));
+                    j++;
+                    while(j < files[i].length()) {
+                        if(Character.isDigit(files[i].charAt(j))) {
+                            num.append(files[i].charAt(j));
+                            j++;
+                        }
+                        else
+                            break;
+                    }
+                    break;
+                }
+            }
+            int len = head.length();
+            head.delete(len - 1, len);
+            vec.add(new File(head.toString(), num.toString(), files[i]));
+        }
+        vec.sort(new FileComparator());
+        answers = new String[vec.size()];
+        for(int i = 0; i < vec.size(); i++) {
+            answers[i] = vec.get(i).name;
+        }
+        return answers;
+    }
+}
+
+
